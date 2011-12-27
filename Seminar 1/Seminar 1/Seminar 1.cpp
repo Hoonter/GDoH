e18c5a6f04439a29c4421c0e6d46aa86b1f0d4a3
Hoonter <Hoonter@yandex.ru>
2011-12-28 01:25:22 +0700

    Seminar 1 full.

---
+++ Seminar 1/Seminar 1/Seminar 1.cpp
@@ -0,0 +1,23 @@
+// Seminar 1.cpp: главный файл проекта.
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    double z1, z2, x, y;
+	cout << "Vedite znachenie X" << endl;
+	cin >> x;
+	cout << "Vedite znachenie Y" << endl;
+	cin >> y;
+	z1 = (pow(cos(x), 4)) + (pow(sin(y), 2))  + ((1/4) * (pow(sin(2 * x), 2) )) - 1;
+	z2 = sin(y + x) * sin (y - x);
+	cout << z1 << endl;
+	cout << z2 << endl;
+	system ("PAUSE");
+    return 0;
+}
